cb26141094483e638ab66e9cc3951c60f55c8440
jyoon00-cloud <ljyoon001126@gmail.com>
2025-10-22 15:38:55 +0900

    ✨ Feat: Add auth middleware 인증 미들웨어 추가

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import prisma from '../db/prismaClient';
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const { accessToken } = req.cookies;
+    if (!accessToken) {
+      throw new Error('로그인 정보가 없습니다.');
+    }
+    const payload = jwt.verify(accessToken, 'YOUR_SECRET_KEY');
+    const study = await prisma.study.findUnique({
+      where: { id: payload.studyId },
+    });
+    if (!study) {
+      return res.status(401).json({ message: '해당하는 스터디가 없습니다' });
+    }
+    req.study = study;
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(419).json({ message: '로그인이 만료되었습니다' });
+    }
+    next(error);
+  }
+};
