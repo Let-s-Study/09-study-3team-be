5d6903942552863e463d814d44e1c378127f25ba
bean-baek <suhyunl1989@gmail.com>
2025-10-24 10:14:38 +0900

    Auth merg

--- server/src/Errors/db/middlewares/validators/study.validator.js
+++ server/src/Errors/db/middlewares/validators/study.validator.js
@@ -5,6 +5,6 @@ export const createStudySchema = z.object({
   title: z.string().trim().min(1, '스터디 제목은 필수입니다'),
   description: z.string().trim().min(2, '소개는 최소 2자리 이상이어야 합니다.'),
   password: z.string().min(4, '비밀번호는 최소 4자리 이상이어야 합니다.'),
-  background: z.string().min(1, '배경선택은 필수 입력 항목입니다.')
+  background: z.string().min(1, '배경 선택은 필수 입력 항목입니다.')
 });
 
