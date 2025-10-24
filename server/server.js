e860edae88c6d07c49e61b732890486bf10ad3d5
bean-baek <suhyunl1989@gmail.com>
2025-10-24 09:20:44 +0900

    merg

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/v1/health', (req, res) => {
+app.get('/api', (req, res) => {
   res.status(200).json({
     success: true,
     message: '✅ Server is healthy!',
